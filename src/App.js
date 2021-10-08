9e85c85dcefd2d4e77209e5bce4994af9198716b
jomo1-1 <89543295+jomo1-1@users.noreply.github.com>
2021-10-07 22:08:55 -0700

    App.js

--- src/App.js
+++ src/App.js
@@ -40,9 +40,9 @@ function App() {
       <div className="auth-wrapper">
         <div className="auth-inner">
           <Switch>
-            <Route exact path='/' component={Login} />
-            <Route path="/sign-in" component={Login} />
-            <Route path="/sign-up" component={SignUp} />
+            <Route exact path='./' component={Home} />
+            <Route path="./sign-in" component={Login} />
+            <Route path="./sign-up" component={SignUp} />
           </Switch>
         </div>
       </div>
